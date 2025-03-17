b0c55423f4eddb0ee83cac6e0adf808ce94cff2d
gaye95 <89346921+gaye95@users.noreply.github.com>
2025-03-17 12:12:04 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Permet d'exposer sur 0.0.0.0 automatiquement
+    port: 5173,
     proxy: {
       "/api": {
-        target: "https://server-eedk.onrender.com", // Remplace par l'URL de ton backend
+        target: "https://server-eedk.onrender.com",
         changeOrigin: true,
         secure: false,
       },
